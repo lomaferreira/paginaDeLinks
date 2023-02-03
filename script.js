e5dacecf582a0f28eec7f68bc2f08e3e703f20e0
lomaferreira <lomasantos666@gmail.com>
2023-02-03 16:52:55 -0300

    correction of comments

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 function toggleMode() {
   const html = document.documentElement;
-  html.classList.toggle('light'); //faz a função //
-
-  /* if(html.classList.contains('light')){        perguntar se contén uma class chamada 'light'
-        html.classList.remove('light')             remove a class 'light'
-    }else{
-        html.classList.add('light')
-    }*/
-
+  html.classList.toggle('light'); // perguntar se contén uma class chamada 'light' remove a class 'light'//
   //pegar a imagem//
   const img = document.querySelector('#profile img');
-
   //substituir a imagem//
   if (html.classList.contains('light')) {
     //se tiver light mode, add a img light//
